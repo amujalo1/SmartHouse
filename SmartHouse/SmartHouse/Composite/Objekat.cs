b366f2ada1091ba425a9cdebabdd6fe5cf45bd23
amujalo1 <amujalo1@etf.unsa.ba>
2025-02-04 00:01:00 +0100

    feture: [Objekat.cs] Added method MoveTo and NadjiParentUredjaj

--- SmartHouse/SmartHouse/Composite/Objekat.cs
+++ SmartHouse/SmartHouse/Composite/Objekat.cs
@@ -47,6 +47,42 @@ namespace SmartHouse.Composite
             return null;
         }
 
+        public virtual Objekat? NadjiParentKomponentu(string id)
+        {
+            foreach (var component in _components)
+            {
+                if (component.isEqualId(id))
+                {
+                    return this;
+                }
+
+                if (component is Objekat soba)
+                {
+                    var result = soba.NadjiParentKomponentu(id);
+                    if (result != null) return result;
+                }
+            }
+            return null;
+        }
+
+        public virtual bool MoveTo(string id, Objekat noviObjekat)
+        {
+            var parent = NadjiParentKomponentu(id);
+            if (parent != null)
+            {
+                var component = parent._components.FirstOrDefault(c => c.isEqualId(id));
+                if (component != null)
+                {
+                    parent.Remove(component);
+                    noviObjekat.Add(component);
+                    Console.WriteLine($"Komponenta sa ID: {id} je premještena u {noviObjekat.Naziv}");
+                    return true;
+                }
+            }
+            Console.WriteLine($"Komponenta sa ID: {id} nije pronađena ili nije premještena!");
+            return false;
+        }
+
         public override void iskljuci()
         {
             foreach (var component in _components)
